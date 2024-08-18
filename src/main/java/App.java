dc3058737bd8776133f243cba5a0213401b54550
filtxxd <filtxxd@gmail.com>
2024-08-18 10:20:11 +0300

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("HelloWorld bean comparison: " + (bean1 == bean2)); // должно быть true
+        System.out.println("Cat bean comparison: " + (cat1 == cat2)); // должно быть false
+    }
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,17 @@
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    // Геттер для имени
+    public String getName() {
+        return name;
+    }
+
+    // Сеттер для имени
+    public void setName(String name) {
+        this.name = name;
+    }
+}
